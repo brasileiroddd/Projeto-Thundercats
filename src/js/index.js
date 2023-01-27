aae57da55ae1f4b62da818e2cb85a8e8bf359a7c
brasileiroddd <123287343+brasileiroddd@users.noreply.github.com>
2023-01-27 00:02:01 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoVeja = document.querySelector(".botao-veja");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoVeja.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
